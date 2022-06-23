4cdd63259c030eccfa90beb1297c659683807cd2
abdesselam-arch <abdesselam-arch>
2022-06-23 12:19:35 +0100

    Fourth commit 1219

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -20,7 +20,6 @@
                         </p>
                     </a>
                 </li>
-                @can('user_management_access')
                     <li class="nav-item has-treeview {{ request()->is("admin/permissions*") ? "menu-open" : "" }} {{ request()->is("admin/roles*") ? "menu-open" : "" }} {{ request()->is("admin/users*") ? "menu-open" : "" }}">
                         <a class="nav-link nav-dropdown-toggle" href="#">
                             <i class="fa-fw nav-icon fas fa-users">
@@ -32,7 +31,6 @@
                             </p>
                         </a>
                         <ul class="nav nav-treeview">
-                            @can('permission_access')
                                 <li class="nav-item">
                                     <a href="{{ route("admin.permissions.index") }}" class="nav-link {{ request()->is("admin/permissions") || request()->is("admin/permissions/*") ? "active" : "" }}">
                                         <i class="fa-fw nav-icon fas fa-unlock-alt">
@@ -43,8 +41,6 @@
                                         </p>
                                     </a>
                                 </li>
-                            @endcan
-                            @can('role_access')
                                 <li class="nav-item">
                                     <a href="{{ route("admin.roles.index") }}" class="nav-link {{ request()->is("admin/roles") || request()->is("admin/roles/*") ? "active" : "" }}">
                                         <i class="fa-fw nav-icon fas fa-briefcase">
@@ -55,8 +51,6 @@
                                         </p>
                                     </a>
                                 </li>
-                            @endcan
-                            @can('user_access')
                                 <li class="nav-item">
                                     <a href="{{ route("admin.users.index") }}" class="nav-link {{ request()->is("admin/users") || request()->is("admin/users/*") ? "active" : "" }}">
                                         <i class="fa-fw nav-icon fas fa-user">
@@ -67,11 +61,8 @@
                                         </p>
                                     </a>
                                 </li>
-                            @endcan
                         </ul>
                     </li>
-                @endcan
-                @can('task_management_access')
                     <li class="nav-item has-treeview {{ request()->is("admin/task-statuses*") ? "menu-open" : "" }} {{ request()->is("admin/task-tags*") ? "menu-open" : "" }} {{ request()->is("admin/tasks*") ? "menu-open" : "" }} {{ request()->is("admin/tasks-calendars*") ? "menu-open" : "" }}">
                         <a class="nav-link nav-dropdown-toggle" href="#">
                             <i class="fa-fw nav-icon fas fa-list">
@@ -83,7 +74,6 @@
                             </p>
                         </a>
                         <ul class="nav nav-treeview">
-                            @can('task_status_access')
                                 <li class="nav-item">
                                     <a href="{{ route("admin.task-statuses.index") }}" class="nav-link {{ request()->is("admin/task-statuses") || request()->is("admin/task-statuses/*") ? "active" : "" }}">
                                         <i class="fa-fw nav-icon fas fa-server">
@@ -94,8 +84,6 @@
                                         </p>
                                     </a>
                                 </li>
-                            @endcan
-                            @can('task_tag_access')
                                 <li class="nav-item">
                                     <a href="{{ route("admin.task-tags.index") }}" class="nav-link {{ request()->is("admin/task-tags") || request()->is("admin/task-tags/*") ? "active" : "" }}">
                                         <i class="fa-fw nav-icon fas fa-server">
@@ -106,8 +94,6 @@
                                         </p>
                                     </a>
                                 </li>
-                            @endcan
-                            @can('task_access')
                                 <li class="nav-item">
                                     <a href="{{ route("admin.tasks.index") }}" class="nav-link {{ request()->is("admin/tasks") || request()->is("admin/tasks/*") ? "active" : "" }}">
                                         <i class="fa-fw nav-icon fas fa-briefcase">
@@ -118,8 +104,6 @@
                                         </p>
                                     </a>
                                 </li>
-                            @endcan
-                            @can('tasks_calendar_access')
                                 <li class="nav-item">
                                     <a href="{{ route("admin.tasks-calendars.index") }}" class="nav-link {{ request()->is("admin/tasks-calendars") || request()->is("admin/tasks-calendars/*") ? "active" : "" }}">
                                         <i class="fa-fw nav-icon fas fa-calendar">
@@ -130,12 +114,9 @@
                                         </p>
                                     </a>
                                 </li>
-                            @endcan
                         </ul>
                     </li>
-                @endcan
                 @if(file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php')))
-                    @can('profile_password_edit')
                         <li class="nav-item">
                             <a class="nav-link {{ request()->is('profile/password') || request()->is('profile/password/*') ? 'active' : '' }}" href="{{ route('profile.password.edit') }}">
                                 <i class="fa-fw fas fa-key nav-icon">
@@ -145,7 +126,6 @@
                                 </p>
                             </a>
                         </li>
-                    @endcan
                 @endif
                 <li class="nav-item">
                     <a href="#" class="nav-link" onclick="event.preventDefault(); document.getElementById('logoutform').submit();">
